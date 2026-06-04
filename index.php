84bc1379082fe0dcf0b408a387f917e8c7d8763f
CloudSalander <ruben.as532@gmail.com>
2026-06-04 10:27:38 +0200

    Apparently printing first line

--- index.php
+++ index.php
@@ -3,7 +3,15 @@
 //TODO: validate entry
 $height = intval(readline("Please, enter the T height that you want"));
 
+$firstLine = "";
 
+$firstLine = str_repeat("T",$height);
+if($height%2 == 0) {
+    $middle = intdiv($height,2);
+    $firstLine = substr($firstLine, 0, $middle) . " " . substr($firstLine, $middle);
+}
+
+echo $firstLine.PHP_EOL;
 
 
 ?>
